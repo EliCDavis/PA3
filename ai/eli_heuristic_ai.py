6e4ecc1490656c59b587e775767c69079a8f1fb8
Eli Davis <eli.davis1995@gmail.com>
2015-11-30 02:26:55 -0600

    Figured out doing everything wrong
    We had our is_our_territory() method written incorrectely, as well as
    evaluating states in the proper context.  At the moment this just won 41
    out of 50 times against the attacker AI.

--- ai/eli_heuristic_ai.py
+++ ai/eli_heuristic_ai.py
@@ -16,7 +16,7 @@ def getAction(state, time_left=None):
     # Get the possible actions in this state
     actions = getAllowedActions(state)
 
-    print('Heuristic AI : Player', state.players[state.current_player].name, 'considering', len(actions), 'actions, with time left: ',time_left)
+    #print('Heuristic AI : Player', state.players[state.current_player].name, 'considering', len(actions), 'actions, with time left: ',time_left)
 
     # To keep track of the best action we find
     best_action = None
@@ -32,102 +32,328 @@ def getAction(state, time_left=None):
         current_action_value = 0.0
 
         for i in range(len(successors)):
+
             # Each successor contributes its heuristic value * its probability to this action's value
-            current_action_value += (heuristic(successors[i]) * probabilities[i])
+            h = heuristic(state, successors[i] )
+
+            current_action_value += (h * probabilities[i])
 
         # Store this as the best action if it is the first or better than what we have found
         if best_action_value is None or current_action_value > best_action_value:
             best_action = a
             best_action_value = current_action_value
 
+    our_t = 0
+    opp_t = 0
+    for t in state.board.territories:
+        if is_our_territory(state, t, state.current_player):
+            our_t += 1
+
+        if is_their_territory(state, t, state.current_player):
+            opp_t += 1
+
+    #print("H:" + str(our_t) + "|O:" + str(opp_t))
+    #print(best_action.description(), best_action_value )
+    #print
+
     # Return the best action
     return best_action
 
 
-def heuristic(state):
+def heuristic(original_state, state):
     """Returns a number telling how good this state is"""
 
     #Go through all territory evaluations and determine state based on their attractiveness
-    h = 1000
-    for t in state.board.territories:
-        h -= evalTerritory(state, t)
-       
+    h = 0
+
+    if original_state.turn_type == 'PreAssign':
+        h = pre_assign_heuristic(state, original_state.current_player)
+
+    if original_state.turn_type == 'PrePlace':
+        h = pre_place_heuristic(state, original_state.current_player)
+
+    if original_state.turn_type == 'Place':
+        h = pre_place_heuristic(state, original_state.current_player)
+
+    # Commenting this section out speeds up games much quicker
+    # Having this in the code drastically improved win rates
+    if state.turn_type == 'TurnInCards':
+        h = pre_assign_heuristic(state, original_state.current_player)
+
+    if original_state.turn_type == 'Attack':
+        h = attack_heuristic(state, original_state.current_player)
+
+    if original_state.turn_type == 'Occupy':
+        h = pre_place_heuristic(state, original_state.current_player)
+
+    if original_state.turn_type == 'Fortify':
+        h = pre_place_heuristic(state, original_state.current_player)
 
     return h
+
+
+def attack_heuristic(state, opid):
+
+    h = 0
+
+    for t in state.board.territories:
+
+        if is_our_territory(state, t, opid):
+
+            h += 1 * state.armies[t.id]
+
+        else:
+            h -= 2 * state.armies[t.id]
+
+    # Determine any continent bonuses we deserve
+    for c in state.board.continents:
+        if is_our_continent(state, state.board.continents[c], opid):
+            h += 50
+
+    # Determine any continent bonuses the opponent is receiving
+    for c in state.board.continents:
+        if is_their_continent(state, state.board.continents[c], opid):
+            h -= 100
+
+    return h
+
+
+def pre_place_heuristic(state, opid):
+    """
+    Place armies on territories that border enemies.
+
+    :param state: RiskState
+    :return:
+    """
+
+    h = 0
+
+    for t in state.board.territories:
+
+        if is_our_territory(state, t, opid):
+
+            # Look at all our neighbors.
+            for n in t.neighbors:
+
+                # If our neighbor is our enemey, then we're doing good putting armies here
+                if is_our_territory(state, id_to_terr(state.board, n), opid) == False:
+
+                    # We want at least 2 armies before we start getting a positive score.
+                    h += -2 + state.armies[t.id]
+
+    return h
+
+
+def id_to_terr(board, id):
+
+    for t in board.territories:
+        if t.id == id:
+            return t
+
+
+def pre_assign_heuristic(state, opid):
+
+    """
+    Here we cut out any monopolies the enemy might get by counter picking.
+
+    :param state: RiskState
+    :return:
+    """
+
+    h = 0
+
+
+    # Array of territory Ids that are not owned by a single entity.
+    territories_free = []
+
+    # Get available territories to choose
+    for i in range(len(state.owners)):
+
+        # If the territory does not have an owner add it.
+        if state.owners[i] is None:
+            territories_free.append(i)
+
+    # Determine if the opponent has any continent bonuses
+    # Does it really even matter at this point if we're not going more than one step ahead?
+
+    # Determine if the opponent is one step away from any continent bonuses
+    opponent_step_away = False
+    for c in state.board.continents:
+
+        continent = state.board.continents[c]
+
+        # Array of integers indexed by RiskPlayer ids that correspond to how many territories they own
+        # In the current continent.  The last index in the array represents that no one owns a territory.
+        owners = [0]*(len(state.board.players)+1)
+
+        # go through and figure out who owns what inside the continent
+        for t_index in range(len(state.board.territories)):
+
+            t = state.board.territories[t_index]
+
+            # If this territory is appart of the continent we're currently looking at..
+            if t.id in continent.territories:
+
+                # If its noe add to the None owner represented by the last index.
+                if state.owners[t.id] is None:
+                    owners[len(owners) - 1] += 1
+
+                else:
+                    owners[state.owners[t.id]] += 1
+
+
+        # Opponent is one step away if there is one unowned territory and the rest are owned by a single player
+        if owners[len(owners) - 1] == 1:
+
+            for i in range(len(owners)-1):
+
+                if owners[i] == len(continent.territories)-1 and i != opid:
+                    opponent_step_away = True
+
+    if opponent_step_away:
+        h -= 200
+
+
+    # Determine if we have any continent bonuses
+    for c in state.board.continents:
+        if is_our_continent(state, state.board.continents[c], opid):
+            h += 50
+
+    # Bonus given to less, larger clusters
+    regions = grab_regions(state, opid)
+    h += 50 - (len(regions)*20)
+
+    return h
+
+
+def territories_are_neighbors(board, t_id1, t_id2):
+
+    """
+    Determines whether or not two territories are neighbors to each other
+
+    :param board: RiskBoard
+    :param t_id1: int
+    :param t_id2: int
+    :return: boolean
+    """
+
+    for t in board.territories:
+        if t.id == t_id1:
+            for n in t.neighbors:
+                if n == t_id2:
+                    return True
+
+    return False
+
+
+def is_our_continent(state, continent, opid):
+
+    """
+
+    :param state: RiskState
+    :param continent: RiskContinent
+    :return: boolean
+    """
+
+    for t in continent.territories:
+        if is_our_territory(state, state.board.territories[t], opid) is False:
+            return False
+
+    return True
+
+
+def is_their_continent(state, continent, opid):
+
+    for t in continent.territories:
+        if is_their_territory(state, state.board.territories[t], opid) is False:
+            return False
+
+    return True
 
 
 def evalContinent(state):
     return 0
 
 
-def evalTerritory(state, territoryID):
+def eval_territory(state, territory):
     """
     This ranks territories on how important they are to the AI
     If this is our territory we want to protect it.
     If this is an enemies territory we want to claim it.
 
     :type state: RiskState
-    :type territoryID: int
+    :type territory: int
     """
 
+
     # This is the variable containing how much appeal this territory has to our agent.
-    tAppeal = 0
+    t_appeal = 0
 
     # figure out what continent the territory is in..
     continent = None
 
-    """
+    # Find the continent the terriroty is contained in
     for cont in state.board.continents:
 
-        if territoryID in cont.territories:
-            #assert isinstance(cont, RiskContinent)
-            continent = cont
-    """
+        if territory.id in state.board.continents[cont].territories:
+            continent = state.board.continents[cont]
 
-    # figure out importance of continent..
-    territory = state.board.territories[territoryID.id]
+    # If this belongs to a continent that we own it's more important to us.
+    if is_our_continent(state, continent):
+        t_appeal += 50
 
-    # Figure out how many neighbors it has that are not our territories, and the size of their armies.
-    # This is good for Fortifying!!!
+    # If this territory is completely surrounded by it's friendly territories don't bother with it..
 
     # Grab all our neighbors
     neighbors = territory.neighbors
 
+    neighbors_are_same_owner = True
+
+    for n in neighbors:
+        if state.owners[territory.id] != state.owners[n]:
+            neighbors_are_same_owner = False
+
+    if neighbors_are_same_owner:
+        t_appeal = 0
+
     # Go through each neighbor and look at their armies.
+
     for neighbor in neighbors:
 
         if state.board.territories != None:
 
             # if the neighbor is ours the appeal is greater the more armies we have surrounding this territory
-            if isOurTerritory(state, state.board.territories[neighbor]):
-                tAppeal += state.armies[neighbor]
+            if is_our_territory(state, state.board.territories[neighbor]):
+                t_appeal += state.armies[neighbor]
             else:
-                tAppeal -= state.armies[neighbor]
+                t_appeal -= state.armies[neighbor]
 
-    # 2nd Appeal
-    # Prioritizing Attacking Continents Rules
-    # Don't Spread to Thin!
-    # 
 
-    # Monopoly check on continent it's contained in.
+    return t_appeal
 
-    # If this territory is completely surrounded by our own territories, we don't need to bother with it
-    #tAppeal = 0
 
-    return tAppeal
+def is_our_territory(state, territory, opid):
 
-def isOurTerritory(state, territory):
     """
-    Determines whether or not the territory passed in belongs to our AI given the current state of things
+    Determines whether or not the territory passed in belongs to our AI given the current state of things.
+    Assumes that state is in opponents phase
 
     :param state: RiskState
     :param territory: RiskTerritory
     :return: boolean
     """
-    our_id = state.current_player
 
-    if state.owners[territory.id] == our_id:
+    if state.owners[territory.id] == opid:
         return True
+
+    return False
+
+
+def is_their_territory(state, territory, opid):
+
+    if state.owners[territory.id] != opid and state.owners[territory.id] is not None:
+        return True
+
     return False
 
 
@@ -147,28 +373,27 @@ def grab_chokeholds(state):
 
     # after writing grab_regions and grab_region I thought of a simpler better way...
 
-     # Go through all territories on board
+    # Go through all territories on board
     for territory in state.board.territories:
 
         # If this terriroty is an enemy territory.
-        if isOurTerritory(state, territory) == False:
+        if is_our_territory(state, territory) == False:
 
             our_territories_it_touches = []
 
             for neighbor in territory.neighbors:
 
-                if isOurTerritory(state, state.board.territories[neighbor] ):
+                if is_our_territory(state, state.board.territories[neighbor] ):
 
                     our_territories_it_touches.append( state.board.territories[neighbor] )
 
             if len(our_territories_it_touches) == 1:
                 chokeholds += our_territories_it_touches
 
-    return set(chokeholds)
+    return list(set(chokeholds))
 
 
-
-def grab_regions(state):
+def grab_regions(state, opid):
 
     """
     Goes through and finds clusters of territories.
@@ -184,7 +409,7 @@ def grab_regions(state):
     for territory in state.board.territories:
 
         # If this terriroty is ours lets try creating a region out of it
-        if isOurTerritory(state, territory):
+        if is_our_territory(state, territory, opid):
 
             contained_in_another_region = False
 
@@ -197,14 +422,15 @@ def grab_regions(state):
                         contained_in_another_region = True
 
             # Territory is not already in another region, we can create a new region!
-            if contained_in_another_region == False:
+            if contained_in_another_region is False:
 
-                regions.append(grab_region)
+                regions.append(grab_region(state, territory, None, opid))
+
 
     return regions
 
 
-def grab_region(state, territory):
+def grab_region(state, territory, already_found, opid):
 
     """
     Recursive function that grabs all territories of ours that are touching another one of our territories in a link.
@@ -213,20 +439,31 @@ def grab_region(state, territory):
 
     :param state: RiskState
     :param territory: RiskTerritory
+    :param already_found: RiskTerritory[]
     :return: RiskTerritory[]
     """
 
-    region = [territory]
+    region = []
+
+    if already_found is None or territory not in already_found:
+        region.append(territory)
+
+    if already_found is not None:
+        region += already_found
+
 
     for neighbor in territory.neighbors:
 
-        if isOurTerritory(state, state.board.territories[neighbor] ):
+        if already_found is not None and state.board.territories[neighbor] in already_found:
+            continue
+
+        if is_our_territory(state, state.board.territories[neighbor], opid):
 
             # Add all friendly neighbors
-            region += grab_region(state, state.board.territories[neighbor])
+            region += grab_region(state, state.board.territories[neighbor], region, opid)
 
     # Return a set to avoid duplicates.
-    return set(region)
+    return list(set(region))
 
 
 
